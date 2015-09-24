ebfc008cfb00ea3e0e0639ab983152fd980f7dfc
jorcox <646122@unizar.es>
2015-09-24 18:03:46 +0200

    Changes in HelloController.java

--- src/main/java/es/unizar/webeng/hello/HelloController.java
+++ src/main/java/es/unizar/webeng/hello/HelloController.java
@@ -7,19 +7,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*
+ * This annotation indicates that HelloController takes the 
+ * role of a controller
+ */
 @Controller
 public class HelloController {
    /*
-    * This tag is used to set a default value from properties file
+    * This annotation is used to set a default value from properties file
 	*/
 	@Value("${app.message:Hello World}")
 	private String message;	
 	
    /*
-	* This tag is used by Spring to redirect each client call
+	* This annotation is used by Spring to redirect each client call
 	* to this method
 	*/	
 	@RequestMapping("/")
+	
+   /**
+	* This method assigns the date and the message
+	* to the data structure called model and 
+	* returns the name of the corresponding view, in
+	* this case wellcome.jsp
+	*/
 	public String welcome(Map<String, Object> model) {
 		model.put("time", new Date());
 		model.put("message", message);
