c986b2bc5a941e242c6cedadbfb597205a7b0755
Santi-7 <santi.gil.begue7@gmail.com>
2015-09-24 22:30:20 +0200

    Update HelloController.java

--- src/main/java/es/unizar/webeng/hello/HelloController.java
+++ src/main/java/es/unizar/webeng/hello/HelloController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 	@Value("${app.message:Hello World}")
-	private String message;	
+	private String message;
+	/*
+	 *
+	 */
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
 		model.put("time", new Date());
