d9616a7a57dd2df7693d7051de419f20cc94f4b3
jorcox <646122@unizar.es>
2015-09-25 09:38:25 +0200

    Comments have been added to SystemTest.java and HelloController.java

--- src/test/java/es/unizar/webeng/hello/SystemTests.java
+++ src/test/java/es/unizar/webeng/hello/SystemTests.java
@@ -60,10 +60,11 @@ public class SystemTests {
 	public void testHome() throws Exception {
 		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
 				"http://localhost:" + this.port, String.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());                  // Check if the HTTP status is OK
+		assertEquals(HttpStatus.OK, entity.getStatusCode());                  // Check if the HTTP status is Ok
 		assertTrue("Wrong body (title doesn't match):\n" + entity.getBody(), entity
 				.getBody().contains("<title>Hello"));
 	}
+	
 	
 	@Test
 	public void testCss() throws Exception {
